064f42d3a8a2e3f48f65db1076561eb0c75023fe
DennisNenov <nycdennen@gmail.com>
2014-06-02 17:59:45 -0400

    Created TreeObject and TreeObjectTest class
    1. Deleted the Object class
    2. Created the TreeObject class
    3. Created the TreeObjectTest class--the TreeObject class should be
    fully functional

---
+++ TreeObjectTest.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+public class TreeObjectTest
+{
+	public static void main (String[] args)
+	{
+		TreeObject testObject1 = new TreeObject();
+		ArrayList<Integer> testArray = new ArrayList<Integer>();
+		testArray.add(1);
+		testArray.add(2);
+		testArray.add(3);
+		TreeObject testObject2 = new TreeObject("Object 2", testArray);
+		//Testing Object 1
+		System.out.println("Testing object 1");
+		System.out.println(testObject1.getAttributes().toString()); //"[]"
+		System.out.println(testObject1.getName().toString()); //"Null"
+		testObject1.setAttributes(testArray);
+		System.out.println(testObject1.getAttributes().toString()); //"[1,2,3]"
+		System.out.println(testObject1.getName().toString()); //"Null"
+		testObject2.clearAttributes();
+		System.out.println(testObject1.getAttributes().toString()); //"[]"
+
+		//Testing Object 2
+		System.out.println("Testing object 2");
+		System.out.println(testObject2.getAttributes().toString()); //"[1,2,3]"
+		System.out.println(testObject2.getName().toString()); //"Object 2"
+		testArray.add(4);
+		testArray.add(5);
+		testObject2.setAttributes(testArray);
+		System.out.println(testObject2.getAttributes().toString()); //"[1,2,3,4,5]"
+		System.out.println(testObject2.getName().toString()); //"Object 2"
+		testObject2.clearAttributes();
+		System.out.println(testObject2.getAttributes().toString()); //"[]"
+
+	}
+}
